47b5b77fcd2c81b36ba69eb8594a2ebdf419674e
snowflakes640 <maliha5640@gmail.com>
2024-11-23 20:50:13 +0600

    Assignment1: Simple Library (Almost)

--- JS_adv/Assign1_library.js
+++ JS_adv/Assign1_library.js
@@ -2,75 +2,119 @@ let myLibrary = []
 const container = document.getElementById("container")
 const dialog = document.querySelector("dialog")
 const addBtn = document.getElementById("addBtn")
-//const clsBtn = document.querySelector("dialog button")
-const bookForm = document.getElementById("bookForm")
-
+const clsBtn = document.getElementById("clsBtn")
+const form = document.getElementById("bookForm")
+let changeStat
 
 function Book(name, author, pages, status){
     this.name = name;
     this.author = author
     this.pages = pages
     this.status = status
+    this.bookID = Book.bookID++
     this.info = function(){
-        return [this.name, this.author, this.pages, this.status]
-    } 
-
+        return [this.name, this.author, this.pages, this.status, this.bookID]
+    }
 }
 //console.log(book1.info())
+Book.bookID = 1
 
 function addBookToLibrary(name, author, pages, status){    
    const new_book = new Book(name, author, pages, status)
    myLibrary.push(new_book)
 }
 
-function displayBooks() {
-    myLibrary.forEach (book => {
-        const book_info = book.info()
+addBookToLibrary("A Kite Runner", "Khaled", "280", true)
+addBookToLibrary("The Litle Coffee Shop", "Deborah", "288", false)
+showLibrary()
+
+
+function showLibrary() {
+    container.innerHTML = " "
+    myLibrary.forEach(book => {
         const cardContainer = document.createElement("div")
+
+        const createChild = (tag, content) => {
+            const contentTag = document.createElement(tag)
+            contentTag.innerText = content
+            cardContainer.appendChild(contentTag)
+        }
+
+        let readStatus
+
+        if(book.status){
+            readStatus = "Read"
+        } else{
+            readStatus = "Not Read"
+        }
+
+
+        createChild("h1", book.name)
+        createChild("h3", book.author)
+        createChild("p", `Number of pages: ${book.pages}`)
+        createChild("p", `Reading status: ${readStatus}`)
         
-        const title = document.createElement("h3")
-        title.innerText = `Book Title: ${book_info[0]}`
 
-        const author = document.createElement("p")
-        author.innerText = `Author Name: ${book_info[1]}`
+        changeStat = document.createElement("button")
+        changeStat.setAttribute("class", "Button")
+        changeStat.setAttribute("id", book.bookID)
+        changeStat.innerText = "Change Status"
+        changeStat.addEventListener("click", (event) => {
+            console.log(event.target)
+            myLibrary.forEach(book  => {
+                if (book.bookID === parseInt(event.target.id)) {
+                    if( book.status) {
+                        book.status = false
+                    } else {
+                        book.status = true
+                    }
+                }
+            })
+            showLibrary()
+        })
 
-        const pages = document.createElement("p")
-        pages.innerText = `Number of Pages: ${book_info[2]}`
-
-        const status = document.createElement("p")
-        status.innerText = `Reading Status: ${book_info[3]}`
 
         const dltBtn = document.createElement("button")
-        dltBtn.setAttribute("id", )
+        dltBtn.setAttribute("id", book.bookID)
+        dltBtn.setAttribute("class", "Button")
+        dltBtn.innerText = "Remove Book"
+        dltBtn.addEventListener("click", (event) => {
+             myLibrary = myLibrary.filter(b => b.bookID !== parseInt(event.target.id))
+             showLibrary()
+        })
+
 
         container.appendChild(cardContainer)
         cardContainer.setAttribute("class", "card")
-        cardContainer.appendChild(title)
-        cardContainer.appendChild(author)
-        cardContainer.appendChild(pages)
-        cardContainer.appendChild(status)
-
+        cardContainer.appendChild(changeStat)
+        cardContainer.appendChild(dltBtn)
     })
 }
 
 addBtn.addEventListener("click", () => {
     dialog.showModal()
 })
+
+clsBtn.addEventListener("click", (event)=> {
+    event.preventDefault()
+    dialog.close()
+})
+
+
 bookForm.addEventListener("submit", (event) => {
     event.preventDefault()
     const inp_title = document.getElementById("title").value
     const inp_author = document.getElementById("author").value
     const inp_pages = document.getElementById("pages").value
-    const inp_status = document.querySelector('input[name="status"]:checked')?.value;
+    const inp_status = document.querySelector('input[name="status"]:checked')?.value || false;
     console.log(inp_title, inp_author, inp_pages, inp_status)
-
     addBookToLibrary(inp_title, inp_author, inp_pages, inp_status)
+    showLibrary()
+    form.reset()
     dialog.close()
-    displayBooks()
 })
 
-addBookToLibrary("A Kite Runner", "Khaled", "280", "Completed")
-addBookToLibrary("The Litle Coffee Shop", "Deborah", "288", "Reading")
+
 console.log(myLibrary)
 //displayBooks()
 
