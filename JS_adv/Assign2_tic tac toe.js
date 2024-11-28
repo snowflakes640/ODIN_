415906a759628323881d1a419d8f1759b7e2198c
snowflakes640 <maliha5640@gmail.com>
2024-11-28 20:18:56 +0600

    Completed the draft of the whole game

--- JS_adv/Assign2_tic tac toe.js
+++ JS_adv/Assign2_tic tac toe.js
@@ -3,14 +3,17 @@ let gameBoard = (function(){        //using iife factory function
     let board = [["_", "_", "_"],
             ["_", "_", "_"],
             ["_", "_", "_"]]
-    
+    // let board = [["1", "2", "3"],
+    //         ["4", "5", "6"],
+    //         ["7", "8", "9"]]
+
     const displayBoard = () => {     
         board.forEach(row => {
         console.log("|  " + row.join("  |  ") + "  |")
         console.log("--------------------")  
         })}
     return {board, displayBoard}
-        }) ()
+    }) ()
 // let gameBoard = {                //creating direct object and visualization function
 //     board: [["_", "_", "_"],
 //             ["_", "_", "_"],
@@ -21,8 +24,7 @@ let gameBoard = (function(){        //using iife factory function
 //     console.log("|  " + row.join("  |  ") + "  |")
 //     console.log("--------------------")  
 // })}
-
-//ameBoard.displayBoard()
+//gameBoard.displayBoard()
 
 // creating players
 function createPlayer(name, marker, score){
@@ -30,6 +32,7 @@ function createPlayer(name, marker, score){
 }
 const player1 = createPlayer("player1", "X", 0)
 const player2 = createPlayer("player2", "O", 0)
+
 
 
 // Taking position and setting them on the board
@@ -58,13 +61,71 @@ function playerTurn(player, board){
 }
 
 
-// play one round
-function playRound(){
-    playerTurn(player1, gameBoard)
-    playerTurn(player2, gameBoard)
-        return gameBoard.displayBoard()
 
+// Game rules
+function gameRules(gameBoard){
+    for (let row of gameBoard.board) { 
+        if(row[0] == row[1] &&
+           row[1] == row[2] &&
+           row[0] != "_"){
+            console.log(" We got a row match !!!")
+            return true
+        }
+    }
+    for(let i=0; i<3; i++){
+           if(gameBoard.board[0][i] == gameBoard.board[1][i] &&
+            gameBoard.board[1][i] == gameBoard.board[2][i] &&
+            gameBoard.board[0][i] != "_"){
+            console.log(" We got a column match !!!")
+            return true
+           }
+    }
+    if(gameBoard.board[0][0] == gameBoard.board[1][1] &&
+        gameBoard.board[1][1] == gameBoard.board[2][2] &&
+        gameBoard.board[0][0] !== "_" ) {
+        console.log(" We got a diagonal match !!!")
+        return true
+    }
+    if(gameBoard.board[0][2] == gameBoard.board[1][1] &&
+        gameBoard.board[1][1] == gameBoard.board[2][0] &&
+        gameBoard.board[0][2] !== "_" ) {
+        console.log(" We got a anti-diagonal match !!!")
+        return true
+    }
+
+    console.log("no match")
+    return false
 }
 
-playRound()
+
+
+// play one round
+function playRound(){
+    if(gameRules(gameBoard)) {
+        console.log(`We found a winner: ${player2.name}`)
+        return 555
+    } else {
+        console.log("continue")
+        playerTurn(player1, gameBoard)
+    }
+    if(gameRules(gameBoard)) {
+        console.log(`We found a winner: ${player1.name}`)
+        return 555
+    } else {
+        console.log("continue")
+        playerTurn(player2, gameBoard)
+    }
+        return gameBoard.displayBoard()
+}
+
+
+
+//The full game play
+while (true) {
+    let end = playRound()
+    if (end === 555) {
+        gameBoard.displayBoard()
+        break
+    } 
+  }
 
